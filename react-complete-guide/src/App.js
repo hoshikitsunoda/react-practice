21e0ddd2e2a2570d8fd2e6b791362a3a2acdd1da
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-20 20:10:25 -0800

    Understanding and Using state completed.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,23 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hosh', age: 33 },
+      { name: 'Ash', age: 31 },
+      { name: 'shiru', age: 2 }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person name="hosh" age="33" />
-        <Person name="ash" age="31">My Hobbies: 柔術</Person>
-        <Person name="shiru" age="2" />
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: 柔術</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
