8c3cdd9ce009f5eeed8dadcd5e76658729a3c77b
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-20 20:56:50 -0800

    Passing method references between components.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -12,12 +12,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked!')
     // DON'T DO THIS: this.state.persons[0].name = 'Hoshiki'
     this.setState({
       persons: [
-        { name: 'Hoshiki', age: 33 },
+        { name: newName, age: 33 },
         { name: 'Ash', age: 31 },
         { name: 'shiru', age: -22 }
       ]
@@ -29,10 +29,17 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: 柔術</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Hoshiki!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age} />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Seiki')} >My Hobbies: 柔術</Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work?'))
