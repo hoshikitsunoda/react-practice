7d667158e68c3ee229c3735ddf1f3acdab1855cd
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-21 17:43:25 -0800

    Added styling using both inline and stylesheet.

--- react-assignment-1/assignment-1/src/UserInput/UserInput.js
+++ react-assignment-1/assignment-1/src/UserInput/UserInput.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 const UserInput = (props) => {
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    textAlign: 'center'
+  }
+
   return (
     <div>
       <input
+        style={style}
         type="text"
         onChange={props.changed}
         value={props.value} />
--- react-assignment-1/assignment-1/src/UserOutput/UserOutput.js
+++ react-assignment-1/assignment-1/src/UserOutput/UserOutput.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import './UserOutput.css'
 
 const UserOutput = (props) => {
   return (
-    <div>
+    <div className="username">
       <p>User: {props.userName}</p>
       <p>Helloooooo</p>
     </div>
