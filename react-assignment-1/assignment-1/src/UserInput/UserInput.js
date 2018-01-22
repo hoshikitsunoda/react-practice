1fe3f7a8dc4504db2e36aa7d386d8154394c6414
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-21 16:58:51 -0800

    Added input to UserInput and p tags to UserOutput.

--- react-assignment-1/assignment-1/src/UserInput/UserInput.js
+++ react-assignment-1/assignment-1/src/UserInput/UserInput.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
 const UserInput = () => {
-  
+  return (
+    <div>
+      <input type="text"/>
+    </div>
+  )
 }
+
+export default UserInput
--- react-assignment-1/assignment-1/src/UserOutput/UserOutput.js
+++ react-assignment-1/assignment-1/src/UserOutput/UserOutput.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
 const UserOutput = () => {
-
+  return (
+    <div>
+      <p>Hello</p>
+      <p>World</p>
+    </div>
+  )
 }
+
+export default UserOutput
