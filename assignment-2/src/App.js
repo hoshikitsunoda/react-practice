70edf3ee18be712520605ec221745635971b6b90
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-22 21:54:08 -0800

    Create an input field with a change listener which outputs the length of the entered text finished.

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    input: ''
+  }
+
+  inputChangeHandler = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          type="text"
+          onChange={this.inputChangeHandler}
+          value={this.state.input} />
+        <p>{this.state.input.length}</p>
       </div>
     );
   }
