09add43845c5672b2f933cd333eb19dc2cf9ccd9
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-22 21:26:11 -0800

    Flexible lists completed.

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -20,14 +20,23 @@ class App extends Component {
     this.setState({ persons: persons })
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Hosh', age: 33 },
-        { name: event.target.value, age: 31 },
-        { name: 'shiru', age: -22 }
-      ]
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id
     })
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    // const person = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons]
+    persons[personIndex] = person
+
+    this.setState({ persons: persons })
   }
 
   togglePersonsHandler = () => {
@@ -54,7 +63,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
