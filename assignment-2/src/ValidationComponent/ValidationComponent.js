f819bea8530504b43c7c63893c55bc029961aaef
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-22 22:06:53 -0800

    Output text too short or text too long depending on the text length.

--- assignment-2/src/ValidationComponent/ValidationComponent.js
+++ assignment-2/src/ValidationComponent/ValidationComponent.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const ValidationComponent = (props) => {
+  let paragraph = 'Text long enough'
+
+  if(props.inputLength <= 5) {
+    paragraph = 'Text too short'
+  }
+
   return (
     <div>
-      <p>hello</p>
+      <p>{paragraph}</p>
     </div>
   )
 }
