6533ba06a77fc48b818287a9afbb262d0f6e0a29
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-01-26 18:48:39 -0800

    Using references(ref) completed.

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -16,14 +16,22 @@ class Person extends Component {
 
   componentDidMount() {
     console.log('[Person.js] Inside componentDidMount()');
+    if(this.props.position === 0) {
+      this.inputElement.focus()
+    }
   }
+
   render() {
     console.log('[Person.js] Inside render()');
     return (
       <Aux>
         <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>
         <p>{this.props.children}</p>
-        <input type="text" onChange={this.props.changed} value={this.props.name}/>
+        <input
+          ref={(inp) => { this.inputElement = inp }}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name} />
       </Aux>
     )
     // return [
