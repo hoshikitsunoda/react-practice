a17b58f147373c4410c326c607d0393e79c477ca
Hoshiki Tsunoda <liquidohr@gmail.com>
2018-02-10 21:42:39 -0800

    semi-commas removed.

--- assignment-2/src/App.js
+++ assignment-2/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import ValidationComponent from './ValidationComponent/ValidationComponent'
 import CharComponent from './CharComponent/CharComponent'
 
